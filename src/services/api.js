6287b0d926ca8f5ba47c108eb82443b959aa69c5
Leonardo Lavigne <devleolavigne@gmail.com>
2020-03-29 01:27:46 -0300

    add api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://be-the-hero-backend-alpha.herokuapp.com/"
+  baseURL: "http://localhost:3333"
 });
 
 export default api;
